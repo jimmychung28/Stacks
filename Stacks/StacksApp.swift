8732cb061e78d6942b394d703a0f72dd9f3a8f07
Jimmy Chung <jimmy46@hotmail.ca>
2023-03-21 19:52:08 -0400

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Jimmy Chung on 2023-03-21.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
